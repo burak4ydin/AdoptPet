289c835f49a949593160cff8cf2ed700a075ad2b
Burak Aydın <brkydn@icloud.com>
2022-12-16 18:49:40 +0300

    search function, other pages ok

--- Controllers/AdoptController.cs
+++ Controllers/AdoptController.cs
@@ -1,13 +1,104 @@
+using AdoptPetProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdoptPetProject.Controllers;
 
 public class AdoptController : Controller
 {
+
+
+
+
     // GET
     public IActionResult Index()
     {
         ViewData["Active"] = "List";
+        AdoptPetContext context = new AdoptPetContext();
+
+
+
+        var pets = context.Pets.ToList();
+        var catList = context.Categories.ToList();
+        var users = context.Users.ToList();
+        ViewData["Pets"] = pets;
+        ViewData["Categories"] = catList;
+        ViewData["Users"] = users;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(string keyword,int catId)
+    {
+        
+        ViewData["Active"] = "List";
+
+        AdoptPetContext context = new AdoptPetContext();
+
+         
+        // var terms = keyword.Trim();
+        
+
+        if (keyword != null && catId==0)
+        {
+            // Response.WriteAsync("<script>alert('rrData inserted successfully "+keyword+" "+catId+"')</script>");
+            var pets = context.Pets.Where(q=> q.name.Contains(keyword))
+                .ToList();
+            var catList = context.Categories.ToList();
+            var users = context.Users.ToList();
+            ViewData["Pets"] = pets;
+            ViewData["Categories"] = catList;
+            ViewData["Users"] = users;
+        }
+        else if (keyword == null && catId != 0)
+        {          
+            // Response.WriteAsync("<script>alert('aaData inserted successfully keyword:"+keyword+" catOd:"+catId+"')</script>");
+
+            var pets = context.Pets.Where(q => q.catId == catId).ToList();
+            var catList = context.Categories.ToList();
+            var users = context.Users.ToList();
+            ViewData["Pets"] = pets;
+            ViewData["Categories"] = catList;
+            ViewData["Users"] = users;
+        }
+        else if (keyword != null && catId != 0)
+        {           
+            // Response.WriteAsync("<script>alert('ccData inserted successfully keyword:"+keyword+" catOd:"+catId+"')</script>");
+
+            var pets = context.Pets.Where(q  => q.name.Contains(keyword) && q.catId == catId )
+                .ToList();
+            var catList = context.Categories.ToList();
+            var users = context.Users.ToList();
+            ViewData["Pets"] = pets;
+            ViewData["Categories"] = catList;
+            ViewData["Users"] = users;
+        }
+        else if (keyword == null && catId == 0)
+        {            
+            // Response.WriteAsync("<script>alert('ssData inserted successfully "+keyword+" "+catId+"')</script>");
+
+            var pets = context.Pets.ToList();
+            var catList = context.Categories.ToList();
+            var users = context.Users.ToList();
+            ViewData["Pets"] = pets;
+            ViewData["Categories"] = catList;
+            ViewData["Users"] = users;
+        }
+        
+        return View();
+    }
+    
+    
+    public IActionResult Info(string id)
+    {
+        AdoptPetContext context = new AdoptPetContext();
+        // Response.WriteAsync("<script>alert('Data inserted successfully "+id+"')</script>");
+        ViewData["selectedPet"] = Int32.Parse(id);
+        var pets = context.Pets.ToList();
+        var catList = context.Categories.ToList();
+        var users = context.Users.ToList();
+        ViewData["Pets"] = pets;
+        ViewData["Categories"] = catList;
+        ViewData["Users"] = users;
         return View();
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -74,15 +74,27 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Login()
-    {
-        return View();
-    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult About()
+    {
+
+        ViewData["Active"] = "About";
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        ViewData["Active"] = "Contact";
+
+        return View();
+    }
+    
 }
 
